58c357972bdb07db0c0a838a44a126a9867780e9
Gustavo Scavazza <ggscavazza.java@gmail.com>
2023-11-30 00:23:41 -0300

    Add: adicionando routes e controllers para Perfis e Tipos

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -14,6 +14,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home Page")
 }
 
+// FUNÇÕES PARA USUARIOS
 func TodosUsuarios(w http.ResponseWriter, r *http.Request) {
 	var p []models.Usuario
 	database.DB.Find(&p)
@@ -54,4 +55,90 @@ func EditaUsuario(w http.ResponseWriter, r *http.Request) {
 	json.NewDecoder(r.Body).Decode(&usuario)
 	database.DB.Save(&usuario)
 	json.NewEncoder(w).Encode(usuario)
+}
+
+// FUNÇÕES PARA TIPOS
+func TodosTipos(w http.ResponseWriter, r *http.Request) {
+	var p []models.Tipo
+	database.DB.Find(&p)
+	json.NewEncoder(w).Encode(p)
+}
+
+func RetornaUmTipo(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var tipo models.Tipo
+	database.DB.First(&tipo, id)
+	json.NewEncoder(w).Encode(tipo)
+}
+
+func CriaUmNovoTipo(w http.ResponseWriter, r *http.Request) {
+	var novoTipo models.Tipo
+	json.NewDecoder(r.Body).Decode(&novoTipo)
+	database.DB.Create(&novoTipo)
+	json.NewEncoder(w).Encode(novoTipo)
+}
+
+func DeletaUmTipo(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var tipo models.Tipo
+	database.DB.Delete(&tipo, id)
+	json.NewEncoder(w).Encode(tipo)
+}
+
+func EditaTipo(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var tipo models.Tipo
+	database.DB.First(&tipo, id)
+	json.NewDecoder(r.Body).Decode(&tipo)
+	database.DB.Save(&tipo)
+	json.NewEncoder(w).Encode(tipo)
+}
+
+// FUNÇÕES PARA PERFIS
+func TodosPerfis(w http.ResponseWriter, r *http.Request) {
+	var p []models.Perfil
+	database.DB.Find(&p)
+	json.NewEncoder(w).Encode(p)
+}
+
+func RetornaUmPerfil(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var perfil models.Perfil
+	database.DB.First(&perfil, id)
+	json.NewEncoder(w).Encode(perfil)
+}
+
+func CriaUmNovoPerfil(w http.ResponseWriter, r *http.Request) {
+	var novoPerfil models.Perfil
+	json.NewDecoder(r.Body).Decode(&novoPerfil)
+	database.DB.Create(&novoPerfil)
+	json.NewEncoder(w).Encode(novoPerfil)
+}
+
+func DeletaUmPerfil(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var perfil models.Perfil
+	database.DB.Delete(&perfil, id)
+	json.NewEncoder(w).Encode(perfil)
+}
+
+func EditaPerfil(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var perfil models.Perfil
+	database.DB.First(&perfil, id)
+	json.NewDecoder(r.Body).Decode(&perfil)
+	database.DB.Save(&perfil)
+	json.NewEncoder(w).Encode(perfil)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -15,10 +15,27 @@ func HandleRequest() {
 	r.Use(middleware.ContentTypeMiddleware)
 
 	r.HandleFunc("/", controllers.Home)
+
+	// Rotas da API para USUARIOS
 	r.HandleFunc("/api/usuarios", controllers.TodosUsuarios).Methods("Get")
 	r.HandleFunc("/api/usuarios/{id}", controllers.RetornaUmUsuario).Methods("Get")
 	r.HandleFunc("/api/usuarios", controllers.CriaUmNovoUsuario).Methods("Post")
 	r.HandleFunc("/api/usuarios/{id}", controllers.DeletaUmUsuario).Methods("Delete")
 	r.HandleFunc("/api/usuarios/{id}", controllers.EditaUsuario).Methods("Put")
+
+	// Rotas da API para TIPOS
+	r.HandleFunc("/api/tipos", controllers.TodosTipos).Methods("Get")
+	r.HandleFunc("/api/tipos/{id}", controllers.RetornaUmTipo).Methods("Get")
+	r.HandleFunc("/api/tipos", controllers.CriaUmNovoTipo).Methods("Post")
+	r.HandleFunc("/api/tipos/{id}", controllers.DeletaUmTipo).Methods("Delete")
+	r.HandleFunc("/api/tipos/{id}", controllers.EditaTipo).Methods("Put")
+
+	// Rotas da API para PERFIS
+	r.HandleFunc("/api/perfis", controllers.TodosPerfis).Methods("Get")
+	r.HandleFunc("/api/perfis/{id}", controllers.RetornaUmPerfil).Methods("Get")
+	r.HandleFunc("/api/perfis", controllers.CriaUmNovoPerfil).Methods("Post")
+	r.HandleFunc("/api/perfis/{id}", controllers.DeletaUmPerfil).Methods("Delete")
+	r.HandleFunc("/api/perfis/{id}", controllers.EditaPerfil).Methods("Put")
+
 	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
